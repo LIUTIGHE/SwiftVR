623898460ce34f70f93e76a32d3e0241aed47363
Tianyi Liu <112839499+LIUTIGHE@users.noreply.github.com>
2026-07-30 14:14:02 +0800

    Export training checkpoint helpers

--- swiftvr/training/__init__.py
+++ swiftvr/training/__init__.py
@@ -1,5 +1,12 @@
 """Training-side building blocks for SwiftVR."""
 
+from .checkpoint import (
+    capture_trainable_parameters,
+    load_delta_checkpoint,
+    parameter_update_summary,
+    save_delta_checkpoint,
+    trainable_named_parameters,
+)
 from .forward import (
     SwiftVRTrainingForward,
     WanShiftWindow2DTrainProcessor,
@@ -14,10 +21,15 @@ from .forward import (
 __all__ = [
     "SwiftVRTrainingForward",
     "WanShiftWindow2DTrainProcessor",
+    "capture_trainable_parameters",
     "decode_reae_clip",
     "encode_reae_clip",
     "forward_prompt_free_no_time_training",
+    "load_delta_checkpoint",
+    "parameter_update_summary",
     "prepare_prompt_free_no_time_transformer_for_training",
     "prepare_training_batch",
     "required_pixel_multiple",
+    "save_delta_checkpoint",
+    "trainable_named_parameters",
 ]
